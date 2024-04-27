0bfec549c8997922f30f57e3a19e262dc208f822
uncorepc49 <uncorepc49@gmail.com>
2024-04-27 13:04:43 +0500

    small changes

--- dha_api/main.py
+++ dha_api/main.py
@@ -2,35 +2,36 @@ from .config import redirect_uri, client_id, client_secret
 from .urls import (discord_token_url, get_user_url, list_guilds_url, guild_url, list_channels, get_channel,
                    get_guild_member, list_guild_members, list_guild_roles)
 import requests
+from typing import Dict, List
 
 
 class DHAUser:
     def __init__(self, auth_token: str):
         self.auth_token = auth_token
 
-    def info(self):
+    def info(self) -> Dict:
         return requests.get(get_user_url, headers={'Authorization': f'Bearer {self.auth_token}'}).json()
 
-    def guilds(self):
+    def guilds(self) -> Dict:
         return requests.get(list_guilds_url, headers={'Authorization': f'Bearer {self.auth_token}'}).json()
 
-    def guilds_owner(self):
+    def guilds_owner(self) -> List:
         guilds_list = []
         for item in requests.get(list_guilds_url, headers={'Authorization': f'Bearer {self.auth_token}'}).json():
             if item['owner']:
                 guilds_list.append(item)
         return guilds_list
 
-    def avatar(self):
+    def avatar(self) -> str:
         return f'https://cdn.discordapp.com/avatars/{self.info()['id']}/{self.info()['avatar']}.png'
 
-    def name(self, select='u'):
+    def name(self, select='u') -> str:
         if select == 'u':
             return f'{self.info()['username']}'
         elif select == 'g':
             return f'{self.info()['global_name']}'
 
-    def quick_info(self):
+    def quick_info(self) -> Dict:
         user = self.info()
         data = {
             'id': user['id'],
@@ -42,29 +43,29 @@ class DHAUser:
 
 
 class DHAMember:
-    def __init__(self, bot_token: str, member_id: str, guild_id: str):
+    def __init__(self, bot_token: str, member_id: int, guild_id: int):
         self.bot_token = bot_token
         self.member_id = member_id
         self.guild_id = guild_id
 
-    def info(self):
+    def info(self) -> Dict:
         return requests.get(get_guild_member(self.guild_id, self.member_id),
                             headers={'Authorization': f'Bot {self.bot_token}'}).json()
 
-    def user(self):
+    def user(self) -> Dict:
         return self.info()['user']
 
-    def avatar(self):
+    def avatar(self) -> str:
         user = self.info()['user']
         return f'https://cdn.discordapp.com/avatars/{user['id']}/{user['avatar']}.png'
 
-    def name(self, select='u'):
+    def name(self, select='u') -> str:
         if select == 'u':
             return f'{self.info()['user']['username']}'
         elif select == 'g':
             return f'{self.info()['user']['global_name']}'
 
-    def quick_info(self):
+    def quick_info(self) -> Dict:
         user = self.user()
         data = {
             'id': user['id'],
@@ -74,36 +75,52 @@ class DHAMember:
         }
         return data
 
-    def roles(self):
+    def roles(self) -> Dict:
         return self.info()['roles']
 
 
 class DHAGuild:
-    def __init__(self, guild_id: str, bot_token: str):
+    def __init__(self, guild_id: int, bot_token: str):
         self.guild_id = guild_id
         self.bot_token = bot_token
 
-    def info(self):
+    def info(self) -> Dict:
         return requests.get(guild_url(self.guild_id), headers={'Authorization': f'Bot {self.bot_token}'}).json()
 
-    def icon(self):
+    def icon(self) -> str:
         return f'https://cdn.discordapp.com/icons/{self.guild_id}/{self.info()['icon']}.png'
 
-    def name(self):
+    def name(self) -> str:
         return f'{self.info()["name"]}'
 
-    def list_channels(self):
+    def list_channels(self) -> Dict:
         return requests.get(list_channels(self.guild_id), headers={'Authorization': f'Bot {self.bot_token}'}).json()
 
-    def channel(self, channel_id: str):
+    def channel(self, channel_id: str) -> Dict:
         return requests.get(get_channel(channel_id), headers={'Authorization': f'Bot {self.bot_token}'}).json()
 
-    def list_members(self, limit: int = 5):
+    def list_members(self, limit: int = 5) -> Dict:
         return requests.get(list_guild_members(self.guild_id, limit),
                             headers={'Authorization': f'Bot {self.bot_token}'}).json()
 
-    def list_roles(self):
+    def list_roles(self) -> Dict:
         return requests.get(list_guild_roles(self.guild_id), headers={'Authorization': f'Bot {self.bot_token}'}).json()
+
+    def members_count(self) -> str:
+        return self.info()['approximate_member_count']
+
+    def quick_info(self) -> Dict:
+        data = self.info()
+        response = {
+            'id': data['id'],
+            'name': data['name'],
+            'owner_id': data['owner_id'],
+            'region': data['region'],
+            'nsfw': data['nsfw'],
+            'avatar': f'https://cdn.discordapp.com/icons/{self.guild_id}/{data['icon']}.png',
+            'members_count': data['approximate_member_count']
+        }
+        return response
 
 
 def get_token(code: str) -> str:
@@ -119,6 +136,6 @@ def get_token(code: str) -> str:
                          auth=(client_id, client_secret)).json()['access_token']
 
 
-def get_user_by_code(auth_code: str):
+def get_user_by_code(auth_code: str) -> Dict:
     auth_token = get_token(auth_code)
     return requests.get(get_user_url, headers={'Authorization': f'Bearer {auth_token}'}).json()
